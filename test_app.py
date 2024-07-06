95e2fbefbbdc2aedd7ccbd9f8685189ad47ebbeb
unknown <ankit092000@gmail.com>
2024-07-06 17:26:45 +0530

    test

---
+++ test_app.py
@@ -0,0 +1,25 @@
+import pytest
+from app import app
+import json
+
+
+@pytest.fixture
+def client():
+    return app.test_client()
+
+
+def test_root(client):
+    resp = client.get('/')
+    assert resp.status_code == 404
+
+
+def test_home(client):
+    resp = client.get('/AboutMe')
+    assert resp.status_code == 200
+
+
+def test_predict(client):
+    test_data = {'Gender': 'Male', 'Married': 'Unmarried','Credit_History':'Unclear Debts','ApplicantIncome': 100000,'LoanAmount': 2000000}
+    resp = client.post('/prediction',json = test_data)
+    assert resp.status_code == 200
+    assert resp.json == {'loan_approval_status': 'Rejected'}
